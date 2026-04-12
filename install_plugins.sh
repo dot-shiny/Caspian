0593b009007c5073b4dffbb5855be42b26013fc6
dot-shiny <cyfer2210@gmail.com>
2026-04-12 22:57:17 +0700

    O Lord have mercy

---
+++ install_plugins.sh
@@ -0,0 +1,12 @@
+hyprpm update
+
+hyprpm add https://github.com/hyprwm/hyprland-plugins
+
+hyprpm add https://github.com/VirtCode/hypr-dynamic-cursors
+
+hyprpm enable hyprbars
+hyprpm enable hyprexpo
+hyprpm enable dynamic-cursors
+
+hyprpm reload
+notify-send "All plugins built and loaded sucks"
